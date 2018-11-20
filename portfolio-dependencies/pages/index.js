df9e58a8dfb2a6fefa2f58a4f32360bd9fa4fc4e
cacheUp <bradb26@gmail.com>
2018-11-20 14:33:36 +0300

    added shared component

--- portfolio-dependencies/pages/index.js
+++ portfolio-dependencies/pages/index.js
@@ -1,26 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Header from "../components/shared/Header";
 
 class Index extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome Page</h1>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/portfolio">
-          <a>Portfolio</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/blog">
-          <a>Blogs</a>
-        </Link>
-        <Link href="/cv">
-          <a>CV</a>
-        </Link>
+        <Header />
       </div>
     );
   }
