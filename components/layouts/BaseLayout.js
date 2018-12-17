acc95b2c480c7fd8c57391f20caa4e3401c6456d
cacheUp <bradb26@gmail.com>
2018-12-18 00:23:36 +0300

    reconfig baselayout

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Header from '../shared/Header';
+import React from "react";
+import Header from "../shared/Header";
 
-const BaseLayout = (props) => {
-
+const BaseLayout = props => {
   return (
-    <React.Fragment>
+    <div className="layout-container">
       <Header />
-      {props.children}
-    </React.Fragment>
-  )
-}
+      <main className={`cover ${className}`}>
+        <div className="wrapper">{children}</div>
+      </main>
+    </div>
+  );
+};
 
 export default BaseLayout;
