f0c01dbed5b188d91f0746bf4504d0c5dee9031e
cacheUp <bradb26@gmail.com>
2018-12-31 03:07:32 +0300

    signature and certificate start

--- services/auth0.js
+++ services/auth0.js
@@ -1,6 +1,7 @@
 import auth0 from "auth0-js";
 import Cookies from "js-cookie";
 import jwt from "jsonwebtoken";
+import axios from "axios";
 
 const isClient = typeof window !== "undefined";
 
@@ -37,6 +38,14 @@ class Auth {
     this.auth0.authorize();
   }
 
+  async getJWKS() {
+    const res = await axios.get(
+      "https://dev-0kws86jw.auth0.com/.well-known/jwks.json"
+    );
+    const jwks = res.data;
+    return jwks;
+  }
+
   setSession(authResult) {
     // Set the time that the Access Token will expire at
     const expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
@@ -62,9 +71,15 @@ class Auth {
       });
   }
 
-  verifyToken(token) {
+  async verifyToken(token) {
     if (token) {
-      const decodedToken = jwt.decode(token);
+      const decodedToken = jwt.decode(token, { complete: true });
+      const jwks = await this.getJWKS();
+      const jwk = jwks.keys[0];
+      //BUILD CERTIFICATE
+      let cert = jwk.x5c[0];
+      cert = cert.match(/.{1,64}/g).join("\n");
+      cert = `-----BEGIN CERTIFICATE-----\n${cert}-----END CERTIFICATE-----\n`;
       const expiresAt = decodedToken.exp * 1000;
       return decodedToken && new Date().getTime() < expiresAt
         ? decodedToken
