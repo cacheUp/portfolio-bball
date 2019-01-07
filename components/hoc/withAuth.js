22b6d5e58b468d7adf5d3f874f269efb3d9e7b99
cacheUp <bradb26@gmail.com>
2019-01-08 02:45:03 +0300

    with Auth

--- components/hoc/withAuth.js
+++ components/hoc/withAuth.js
@@ -1,7 +1,24 @@
 import React from "react";
+import BaseLayout from "../layouts/BaseLayout";
+import BasePage from "../shared/BasePage";
 
 export default function(Component) {
   return class withAuth extends React.Component {
+    renderProtectedPage() {
+      const { isAuthenticated, user } = this.props.auth;
+      if (isAuthenticated) {
+        return <Component {...this.props} />;
+      } else {
+        return (
+          <BaseLayout {...this.props.auth}>
+            <BasePage>
+              {" "}
+              <h1> You are not Authenticated. Please login to access </h1>
+            </BasePage>
+          </BaseLayout>
+        );
+      }
+    }
     render() {
       return <Component {...this.props} />;
     }
