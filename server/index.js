ed5934208a6afa644e6bbeb919659980cbac8a03
cacheUp <bradb26@gmail.com>
2019-02-02 18:05:21 +0300

    added middlewar roles to server

--- server/index.js
+++ server/index.js
@@ -26,17 +26,22 @@ app
       res.json(secretData);
     });
 
-    server.get("/api/v1/onlysiteowner", authService.checkJWT, (req, res) => {
-      res.json(secretData);
-    });
-
-    server.use(function(err, req, res, next) {
-      if (err.name === "UnauthorizedError") {
-        res
-          .status(401)
-          .send({ title: "Unauthorized", detail: "Unauthorized Access!" });
+    server.get(
+      "/api/v1/onlysiteowner",
+      authService.checkJWT,
+      authService.checkRole("siteOwner"),
+      (req, res) => {
+        res.json(secretData);
       }
-    });
+    );
+
+    // server.use(function(err, req, res, next) {
+    //   if (err.name === "UnauthorizedError") {
+    //     res
+    //       .status(401)
+    //       .send({ title: "Unauthorized", detail: "Unauthorized Access!" });
+    //   }
+    // });
 
     server.get("*", (req, res) => {
       return handle(req, res);
--- server/services/auth.js
+++ server/services/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
+const namespace = "http://localhost:3000/";
 
 exports.checkJWT = jwt({
   secret: jwksRsa.expressJwtSecret({
@@ -12,3 +13,15 @@ exports.checkJWT = jwt({
   issuer: "https://dev-0kws86jw.auth0.com/",
   algorithms: ["RS256"]
 });
+
+exports.checkRole = role => (req, res, next) => {
+  const user = req.user;
+  if (user && user[namespace + "role"] === role) {
+    next();
+  } else {
+    res.status(401).send({
+      title: "Not Authorized",
+      details: "You are not authorized to access this data"
+    });
+  }
+};
