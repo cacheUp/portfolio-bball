cad9a6cba76b29e120ce8835989f02a7b4a5be20
cacheUp <bradb26@gmail.com>
2019-02-17 13:16:26 +0300

    more simple form fields

--- components/portfolios/PortfolioCreateForm.js
+++ components/portfolios/PortfolioCreateForm.js
@@ -3,14 +3,14 @@ import React from "react";
 export default class PortfolioCreateForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { value: "", description: "", language: "" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
@@ -24,11 +24,36 @@ export default class PortfolioCreateForm extends React.Component {
         <label>
           Name:
           <input
+            name="value"
             type="text"
             value={this.state.value}
             onChange={this.handleChange}
           />
         </label>
+        <label>
+          Description:
+          <textarea
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChangeDescription}
+          />
+        </label>
+        <label>
+          Pick your favorite programming:
+          <select
+            name="language"
+            value={this.state.language}
+            onChange={this.handleChangeLanguage}
+          >
+            <option value="javascript">Javascript</option>
+            <option value="java">Java</option>
+            <option selected value="c++">
+              C++
+            </option>
+            <option value="c#">C#</option>
+          </select>
+        </label>
+
         <input type="submit" value="Submit" />
       </form>
     );
