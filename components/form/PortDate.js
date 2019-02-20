313abde63a94bd48e0f834f998bc06ae278070f8
cacheUp <bradb26@gmail.com>
2019-02-20 15:56:08 +0300

    date value

--- components/form/PortDate.js
+++ components/form/PortDate.js
@@ -1,30 +1,30 @@
 import React from "react";
 import DatePicker from "react-datepicker";
-
+import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
 
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
-class Example extends React.Component {
+export default class PortDate extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      startDate: new Date()
+      dateValue: moment()
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(date) {
     this.setState({
-      startDate: date
+      dateValue: date
     });
   }
 
   render() {
     return (
       <DatePicker
-        selected={this.state.startDate}
+        selected={this.state.dateValue}
         onChange={this.handleChange}
       />
     );
