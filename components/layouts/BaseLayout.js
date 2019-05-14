255c10d8c7913d75abed12029298c730954f5b62
cacheUp <bradb26@gmail.com>
2019-05-14 22:17:44 +0300

    added base blog layout

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,21 +1,25 @@
 import React from "react";
 import Header from "../shared/Header";
+import Head from "next/head";
 
 const BaseLayout = props => {
   const { className, children, isAuthenticated, user } = props;
   const headerType = props.headerType || "default";
 
   return (
-    <div className="layout-container">
-      <Header
-        className={`port-nav-${headerType}`}
-        isAuthenticated={isAuthenticated}
-        user={user}
-      />
-      <main className={`cover ${className}`}>
-        <div className="wrapper">{children}</div>
-      </main>
-    </div>
+    <React.Fragment>
+      <Head>{""}</Head>
+      <div className="layout-container">
+        <Header
+          className={`port-nav-${headerType}`}
+          isAuthenticated={isAuthenticated}
+          user={user}
+        />
+        <main className={`cover ${className}`}>
+          <div className="wrapper">{children}</div>
+        </main>
+      </div>
+    </React.Fragment>
   );
 };
 
