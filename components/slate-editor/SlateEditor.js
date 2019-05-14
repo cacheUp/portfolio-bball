999bc55e6bc5e59b0e66bbc58b757e080d460278
cacheUp <bradb26@gmail.com>
2019-05-14 03:33:06 +0300

    slate editor init

---
+++ components/slate-editor/SlateEditor.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Editor } from "slate-react";
+import { Value } from "slate";
+
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            text: "A line of text in a paragraph."
+          }
+        ]
+      }
+    ]
+  }
+});
+
+export default class SlateEditor extends React.Component {
+  state = {
+    value: initialValue
+  };
+  onChange = ({ value }) => {
+    this.setState({ value });
+  };
+
+  render() {
+    return <Editor value={this.state.value} onChange={this.onChange} />;
+  }
+}
--- pages/blogEditor.js
+++ pages/blogEditor.js
@@ -2,13 +2,14 @@ import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/shared/BasePage";
 import withAuth from "../components/hoc/withAuth";
+import SlateEditor from "../components/slate-editor/SlateEditor";
 
 class BlogEditor extends React.Component {
   render() {
     return (
       <BaseLayout {...this.props.auth}>
         <BasePage className="blog-editor-page" title="Write Your Story">
-          {" "}
+          <SlateEditor />
         </BasePage>
       </BaseLayout>
     );
