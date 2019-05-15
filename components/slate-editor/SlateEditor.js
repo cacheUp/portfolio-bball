83ae29722c9cb7c1c26f2ceaa708976f44ddc644
cacheUp <bradb26@gmail.com>
2019-05-15 04:03:55 +0300

    added renderer

--- components/slate-editor/SlateEditor.js
+++ components/slate-editor/SlateEditor.js
@@ -19,6 +19,14 @@ const initialValue = Value.fromJSON({
   }
 });
 
+function CodeNode(props) {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+}
+
 export default class SlateEditor extends React.Component {
   state = {
     value: initialValue,
@@ -31,12 +39,35 @@ export default class SlateEditor extends React.Component {
   onChange = ({ value }) => {
     this.setState({ value });
   };
+
+  onKeyDown = (event, editor, next) => {
+    // Return with no changes if the keypress is not '&'
+    if (event.key !== "&") return next();
+    // Prevent the ampersand character from being inserted.
+    event.preventDefault();
+    // Change the value by inserting 'and' at the cursor's position.
+    editor.insertText("and");
+  };
+
+  renderBlock = (props, editor, next) => {
+    switch (props.node.type) {
+      case "code":
+        return <CodeNode {...props} />;
+      default:
+        return next();
+    }
+  };
+
   render() {
     const { isLoaded } = this.state;
     return (
       <React.Fragment>
         {isLoaded && (
-          <Editor value={this.state.value} onChange={this.onChange} />
+          <Editor
+            value={this.state.value}
+            onKeyDown={this.onKeyDown}
+            onChange={this.onChange}
+          />
         )}
       </React.Fragment>
     );
