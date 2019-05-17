eb3f0b0b262c838863c985fe610d0a8bd1968eaf
cacheUp <bradb26@gmail.com>
2019-05-18 00:29:09 +0300

    additional slate editor styles

--- components/slate-editor/ControlMenu.js
+++ components/slate-editor/ControlMenu.js
@@ -4,7 +4,7 @@ import React from "react";
 export const ControlMenu = props => {
   return (
     <div className="control-menu">
-      <h1>Write Your Story... </h1>
+      <h1 className="title">Write Your Story... </h1>
       <div className="status-box">Saved</div>
       <Button color="success">Save</Button>
     </div>
