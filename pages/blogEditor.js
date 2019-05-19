19b9ac2b79592236c98a0a240dc6a99e04e4ab44
cacheUp <bradb26@gmail.com>
2019-05-19 20:04:22 +0300

    invoke saveBlog in blogEditor, promise resolved init

--- pages/blogEditor.js
+++ pages/blogEditor.js
@@ -3,6 +3,7 @@ import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/shared/BasePage";
 import withAuth from "../components/hoc/withAuth";
 import SlateEditor from "../components/slate-editor/SlateEditor";
+import { saveBlog } from "../actions";
 
 class BlogEditor extends React.Component {
   constructor(props) {
@@ -17,6 +18,10 @@ class BlogEditor extends React.Component {
     blog.title = heading.title;
     blog.subtitle = heading.subtitle;
     this.setState({ isLoading: true });
+    saveBlog().then(data => {
+      console.log(data);
+      this.setState({ isLoading: false });
+    });
   };
 
   render() {
