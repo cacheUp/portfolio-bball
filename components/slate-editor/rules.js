6c982230b8c2ad557277b18f39933f855eb6d94f
cacheUp <bradb26@gmail.com>
2019-05-20 21:38:52 +0300

    serialize bug fix

--- components/slate-editor/rules.js
+++ components/slate-editor/rules.js
@@ -32,9 +32,19 @@ const rules = [
       if (obj.object == "block") {
         switch (obj.type) {
           case "paragraph":
-            return <p>{children}</p>;
+            return <p>{children} </p>;
           case "block-quote":
             return <blockquote>{children}</blockquote>;
+          case "bulleted-list":
+            return <ul>{children}</ul>;
+          case "heading-one":
+            return <h1>{children}</h1>;
+          case "heading-two":
+            return <h2>{children}</h2>;
+          case "list-item":
+            return <li>{children}</li>;
+          case "numbered-list":
+            return <ol>{children}</ol>;
         }
       }
     }
@@ -54,20 +64,12 @@ const rules = [
     serialize(obj, children) {
       if (obj.object == "mark") {
         switch (obj.type) {
-          case "paragraph":
-            return <p>{children} </p>;
-          case "block-quote":
-            return <blockquote>{children}</blockquote>;
-          case "bulleted-list":
-            return <ul>{children}</ul>;
-          case "heading-one":
-            return <h1>{children}</h1>;
-          case "heading-two":
-            return <h2>{children}</h2>;
-          case "list-item":
-            return <li>{children}</li>;
-          case "numbered-list":
-            return <ol>{children}</ol>;
+          case "bold":
+            return <strong>{children}</strong>;
+          case "italic":
+            return <em>{children}</em>;
+          case "underline":
+            return <u>{children}</u>;
         }
       }
     }
