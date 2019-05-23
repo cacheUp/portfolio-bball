1a9cb2621bb273933ea0127a632abc924306cf4e
cacheUp <bradb26@gmail.com>
2019-05-24 02:25:44 +0300

    updateBlog endpoint init

--- server/controllers/blog.js
+++ server/controllers/blog.js
@@ -50,3 +50,22 @@ exports.createBlog = (req, res) => {
     return res.status(422).send({ message: "Blog is saving" });
   }
 };
+
+exports.updateBlog = (req, res) => {
+  const blogId = req.params.id;
+  const blogData = req.body;
+
+  Blog.findById(blogId, (err, foundBlog) => {
+    if (err) {
+      return res.status(422).send(err);
+    }
+    foundBlog.set(blogData);
+    foundBlog.updatedAt = new Date();
+    foundBlog.save((err, foundBlog) => {
+      if (err) {
+        return res.status(422).send(err);
+      }
+      return res.json(foundBlog);
+    });
+  });
+};
