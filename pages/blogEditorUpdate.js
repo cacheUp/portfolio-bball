bb559f7ff43b4a86d420cd7713559e4157edac95
cacheUp <bradb26@gmail.com>
2019-05-23 22:08:49 +0300

    getInitialProps for blogEdit page

--- pages/blogEditorUpdate.js
+++ pages/blogEditorUpdate.js
@@ -3,11 +3,17 @@ import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/shared/BasePage";
 import withAuth from "../components/hoc/withAuth";
 import SlateEditor from "../components/slate-editor/SlateEditor";
-import { createBlog } from "../actions";
+import { getBlogById } from "../actions";
 
 class BlogEditorUpdate extends React.Component {
-  getInitialProps({ query }) {
+  static async getInitialProps({ query }) {
     const blogId = query.id;
+    try {
+      const blog = await getBlogById(blogId);
+      return { blog };
+    } catch (err) {
+      return { err };
+    }
   }
   constructor(props) {
     super(props);
