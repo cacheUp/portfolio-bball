0cbd45cebb8867fb0e853d9693c651e9e88e6acc
cacheUp <bradb26@gmail.com>
2019-05-26 21:09:43 +0300

    base layout with reactstrap

--- pages/userBlogs.js
+++ pages/userBlogs.js
@@ -2,12 +2,42 @@ import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/shared/BasePage";
 import withAuth from "../components/hoc/withAuth";
+import { Containter, Col, Row } from "reactstrap";
 
 class UserBlogs extends React.Component {
   render() {
     return (
-      <BaseLayout {...this.props.auth}>
-        <BasePage title="I am UserBlogs Page"> </BasePage>
+      <BaseLayout
+        {...this.props.auth}
+        headerType={"landing"}
+        className="blog-listing-page"
+      >
+        <div
+          className="masthead"
+          style={{ backgroundImage: "url('/static/images/home-bg.jpg')" }}
+        >
+          <div className="overlay" />
+          <Container>
+            <div className="row">
+              <div className="col-lg-8 col-md-10 mx-auto">
+                <div className="site-heading">
+                  <h1>Fresh Blogs</h1>
+                  <span className="subheading">Programming, travelling...</span>
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+        <BasePage className="blog-body">
+          <Row>
+            <Col md="6" className="mx-auto">
+              Published Blogs
+            </Col>
+            <Col md="6" className="mx-auto">
+              Draft Blogs
+            </Col>
+          </Row>
+        </BasePage>
       </BaseLayout>
     );
   }
