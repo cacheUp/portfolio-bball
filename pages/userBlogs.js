81f8ee34f8f25250a9ff25098d160dae2e928214
cacheUp <bradb26@gmail.com>
2019-05-26 21:05:15 +0300

    withAuth bug fix

--- pages/userBlogs.js
+++ pages/userBlogs.js
@@ -1,7 +1,7 @@
 import React from "react";
 import BaseLayout from "../components/layouts/BaseLayout";
 import BasePage from "../components/shared/BasePage";
-import { withAuth } from "../components/hoc/withAuth";
+import withAuth from "../components/hoc/withAuth";
 
 class UserBlogs extends React.Component {
   render() {
