926f8f8fa2d82bdbe80ce5c825a588b9d3586904
cacheUp <bradb26@gmail.com>
2019-05-29 19:32:33 +0300

    create status and dropdown options

--- pages/userBlogs.js
+++ pages/userBlogs.js
@@ -28,6 +28,17 @@ class UserBlogs extends React.Component {
     return { published, drafts };
   }
 
+  createStatus(status) {
+    return status === "draft"
+      ? { view: "Publish Story", value: "published" }
+      : { view: "Make a Draft", value: "draft" };
+  }
+
+  dropdownOptions = blog => {
+    const blogStatus = this.createStatus(blog.status);
+    return [{ text: blogStatus.view }, { text: "Delete" }];
+  };
+
   renderBlogs(blogs) {
     return (
       <ul className="user-blogs-list">
@@ -36,7 +47,7 @@ class UserBlogs extends React.Component {
             <Link route={`/blogs/${blog._id}/edit`}>
               <a>{blog.title}</a>
             </Link>
-            <PortButtonDropdown />
+            <PortButtonDropdown items={this.dropdownOptions(blog)} />
           </li>
         ))}
       </ul>
