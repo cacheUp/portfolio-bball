311d347f906d477e5846fac0b62ff01e471a04a9
cacheUp <bradb26@gmail.com>
2019-06-03 09:25:50 +0300

    get initial props in BlogDetail

--- pages/blogDetail.js
+++ pages/blogDetail.js
@@ -4,8 +4,19 @@ import BasePage from "../components/shared/BasePage";
 import { getBlogBySlug } from "../actions";
 
 class BlogDetail extends React.Component {
-  static getInitialProps() {}
+  static getInitialProps({query}) {
+    let blogs = {};
+    const slug = query.slug
+      try {
+        blogs = await getBlogBySlug(slug);
+      } catch (err) {
+        console.error(err);
+      }
+      return { blogs };
+    }
+  
   render() {
+    const {blogs} =this.props
     return (
       <BaseLayout {...this.props.auth}>
         <BasePage title="I am Blog Detail Page"> </BasePage>
