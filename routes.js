d115253d1457d070cdaa620a9a2c736242ce735e
cacheUp <bradb26@gmail.com>
2019-06-07 10:14:59 +0300

    bug fix: Not userBlogs, blogEditor

--- routes.js
+++ routes.js
@@ -3,6 +3,6 @@ const routes = require("next-routes");
 module.exports = routes()
   .add("portfolio", "/portfolio/:id")
   .add("portfolioEdit", "/portfolios/:id/edit")
-  .add("userBlogs", "/blogs/new")
+  .add("blogEditor", "/blogs/new")
   .add("blogDetail", "/blogs/:slug")
   .add("blogEditorUpdate", "/blogs/:id/edit");
