a6c10a782c14808c103a3748b57714a45ba019da
cacheUp <bradb26@gmail.com>
2019-06-08 11:39:32 +0300

    about me page init

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,27 @@ class About extends React.Component {
   render() {
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage title="I am About Page"> </BasePage>
+        <BasePage title="I am About Page">
+          {" "}
+          <Row className="mt-5">
+            <Col md="6">
+              <div className="left-side">
+                <h1 className="title fadein">Hello, Welcome</h1>
+                <h4 className="subtitle fadein">To About Page</h4>
+                <p className="subsubTitle fadein">
+                  Feel free to read short description about me.
+                </p>
+              </div>
+            </Col>
+            <Col md="6">
+              <div className="fadein" id="intro">
+                <p>My name is Bradley Ball </p>
+                <p>Add about me Text</p>
+                <p>Add about me Text</p>
+              </div>
+            </Col>
+          </Row>
+        </BasePage>
       </BaseLayout>
     );
   }
