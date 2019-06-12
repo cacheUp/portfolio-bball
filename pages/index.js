341e06273e53063762d706de942357f4546d1086
cacheUp <bradb26@gmail.com>
2019-06-12 20:36:38 +0300

    added back card

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ class Index extends React.Component {
               <Col md="6">
                 <div className="hero-section">
                   <div className={`flipper ${isFlipping ? "isFlipping" : ""}`}>
-                    <div className="back">
+                    <div className="front">
                       <div className="hero-section-content">
                         <h2> Full Stack Web Developer </h2>
                         <div className="hero-section-content-intro">
@@ -57,6 +57,21 @@ class Index extends React.Component {
                       <img
                         className="image"
                         src="/static/images/section-1.png"
+                      />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
+                    </div>
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
+                        </div>
+                      </div>
+                      <img
+                        className="image"
+                        src="/static/images/section-2.png"
                       />
                       <div className="shadow-custom">
                         <div className="shadow-inner"> </div>
