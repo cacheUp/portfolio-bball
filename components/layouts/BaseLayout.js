19dae7ba01ec2b54e479f0b8a23c3917f6153ca7
cacheUp <bradb26@gmail.com>
2019-06-16 16:42:38 +0300

    added more meta tags for SEO

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -26,6 +26,19 @@ const BaseLayout = props => {
           name="keywords"
           content="bradley ball, bradley ball porfolio, bradley ball developer, bradley, bradley portfolio"
         />
+
+        <meta
+          property="og:title"
+          content="Bradley Ball - programmer,devoloper, blogger"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:locale" content="en_EU" />
+        <meta property="og:url" content="http://localhost:3000" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="My name is Bradley Ball and I am a software engineer. I love solving problems and collaberating with teammates to get the job done. React is my favorite frontend framework and nodejs is my preferred backend technology."
+        />
         <script src="https://kit.fontawesome.com/b682f54eb6.js" />
         <link
           href="https://fonts.googleapis.com/icon?family=Material+Icons"
