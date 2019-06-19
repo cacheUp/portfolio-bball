7493a2bab07658e2580a8ee6e4d14468b03e8db7
cacheUp <bradb26@gmail.com>
2019-06-19 09:52:13 +0300

    check for production

--- env-config.js
+++ env-config.js
@@ -1,3 +1,10 @@
 const prod = process.env.NODE_ENV === "production";
 
-module.exports = {};
+module.exports = {
+  "proccess.env.BASE_URL": prod
+    ? "https://portfolio-bball.herokuapp.com"
+    : "http://localhost:3000",
+  "process.env.NAMESPACE": prod
+    ? "https://portfolio-bball.herokuapp.com"
+    : "http://localhost:3000"
+};
