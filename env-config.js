f463224bb75620be197f08175e9c39fe97e744b2
cacheUp <bradb26@gmail.com>
2019-06-20 12:21:48 +0300

    bug fix: PORT

--- env-config.js
+++ env-config.js
@@ -7,5 +7,6 @@ module.exports = {
   "process.env.NAMESPACE": prod
     ? "https://portfolio-bball.herokuapp.com"
     : "http://localhost:3000",
-  "process.env.CLIENT": "H5Z2uRcCJFpz2A8JDXEqWh4M91NH5Axl"
+  "process.env.CLIENT": "H5Z2uRcCJFpz2A8JDXEqWh4M91NH5Axl",
+  "process.env.PORT": "https://portfolio-bball.herokuapp.com"
 };
