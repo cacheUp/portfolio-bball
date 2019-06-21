f0b233973712366b37fdc0e3f0de2d463d694075
cacheUp <bradb26@gmail.com>
2019-06-21 19:04:33 +0300

    added longer jwk requests per minute

--- server/services/auth.js
+++ server/services/auth.js
@@ -6,7 +6,7 @@ exports.checkJWT = jwt({
   secret: jwksRsa.expressJwtSecret({
     cache: true,
     rateLimit: true,
-    jwksRequestsPerMinute: 10, // Default value
+    jwksRequestsPerMinute: 50, // Default value
     jwksUri: "https://dev-0kws86jw.auth0.com/.well-known/jwks.json" // Optional
   }),
   audience: "H5Z2uRcCJFpz2A8JDXEqWh4M91NH5Axl",
