4ad5b59fb3246cf67297b84409898c94fda8ae79
cacheUp <bradb26@gmail.com>
2019-06-22 20:19:06 +0300

    more css webpack config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@ const withSass = require("@zeit/next-sass");
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(
   withSass({
-    webpack() {
-      config, { dev };
+    webpack(config, { dev }) {
+      if (config.mode === "production") {
+        if (Array.isArray(config.optimization.minimizer)) {
+          config.optimization.minimizer.push(new OptimizeCSSAssestsPlugin());
+        }
+      }
+      return config;
     }
   })
 );
