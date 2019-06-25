b96b2d47561bc1e45c6b2c2a28e9eddb1fdb9d6f
cacheUp <bradb26@gmail.com>
2019-06-25 23:46:47 +0300

    changed Namespace config

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
   DB_URI:
-    "mongodb+srv://bradb26:Trophybox1@cluster0-rrbed.mongodb.net/portfolio-bball?retryWrites=true"
+    "mongodb+srv://bradb26:Trophybox1@cluster0-rrbed.mongodb.net/portfolio-bball?retryWrites=true",
+  NAMESPACE: "https://portfolio-bball.herokuapp.com"
 };
