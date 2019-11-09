4f686fe6a8ca030c0669e7d947b9ac765dfce1c3
cacheUp <bradb26@gmail.com>
2019-11-09 17:23:08 +0300

    remove cache

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  DB_URI:
-    "mongodb+srv://bradb26:Trophybox1@cluster0-rrbed.mongodb.net/portfolio-bball?retryWrites=true",
-  NAMESPACE: "https://www.bradleyball.dev",
-  ACCESS_KEY_ID: "AKIA2ANPHZ2MGG7EZ6E3",
-  SECRET_ACCESS_KEY: "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"
-};
